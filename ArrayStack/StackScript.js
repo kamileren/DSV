f67386b0c9cfa9f1e9d9ff899e150bb9565bab4a
b4shk1r <azmtcnd@icloud.com>
2023-12-20 10:22:23 -0500

    Started ArrayStack

---
+++ ArrayStack/StackScript.js
@@ -0,0 +1,61 @@
+const arrayStack = new ArrayStack();
+const elementInput = document.getElementById('element-input');
+const arrayDisplay = document.getElementById('array-display');
+const sidebar = document.getElementById('sidebar');
+    
+
+document.getElementById('push-btn').addEventListener('click', () => {
+    arrayStack.push(elementInput.value);
+    updateDisplay();
+});
+
+document.getElementById('pop-btn').addEventListener('click', () => {
+    arrayStack.pop();
+    updateDisplay();
+});
+
+document.getElementById('make-array-btn').addEventListener('click', () => {
+
+    for (let i = 0; i < 10; i++) {
+        arrayStack.push(i);
+    }
+    updateDisplay();
+    //toggleSidebar(true); // Toggle sidebar when array is created
+});
+
+function updateDisplay() {
+    console.log(arrayStack)
+    arrayDisplay.textContent = 'ArrayStack: [' + arrayStack.data.join(', ') + ']';
+    displayArray();
+ }
+ function displayArray() {
+    const arrayTitle = document.getElementById('array-stack-title');
+    arrayTitle.innerHTML = '';
+
+    const heading = document.createElement('h2');
+    heading.textContent = 'arrayStack Visualization';
+    arrayTitle.appendChild(heading);
+
+    const arrayDisplay = document.getElementById('array-display');
+    arrayDisplay.innerHTML = ''; // Clear existing content
+
+    arrayStack.data.forEach((element) => {
+        // Skip undefined elements
+        if (element !== undefined) {
+            const elementContainer = document.createElement('div');
+            elementContainer.className = 'array-element';
+            elementContainer.textContent = `${element}`;
+            arrayDisplay.appendChild(elementContainer);
+        }
+    });
+}
+document.getElementById('clear-array-btn').addEventListener('click', () => {
+    
+    arrayStack.clear();
+
+    // Update display
+    updateDisplay();
+    //toggleSidebar(false); // Revert sidebar when array is cleared
+});
+
+updateDisplay()
