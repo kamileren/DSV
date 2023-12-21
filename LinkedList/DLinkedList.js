e865c201918c8ebf6ce4da24d9ca3bc29589a21d
avsakamil@gmail.com <avsakamil@gmail.com>
2023-12-20 22:57:10 -0500

    Doubly Linked List Started

---
+++ LinkedList/DLinkedList.js
@@ -0,0 +1,22 @@
+class Node
+{
+    constructor(data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class LinkedList
+{
+    constructor()
+    {
+        this.head = null;
+        this.tail = null;
+        this.n = 0;
+    }
+
+    
+}
