33da7755a319bfcc3ef38971c6225bba3392bb6a
b4shk1r <azmtcnd@icloud.com>
2023-12-20 21:29:18 -0500

    Stack visualized

--- ArrayStack/StackScript.js
+++ ArrayStack/StackScript.js
@@ -22,38 +22,61 @@ document.getElementById('make-array-btn').addEventListener('click', () => {
     updateDisplay();
     //toggleSidebar(true); // Toggle sidebar when array is created
 });
-
 function updateDisplay() {
     console.log(arrayStack)
     arrayDisplay.textContent = 'ArrayStack: [' + arrayStack.data.join(', ') + ']';
     displayArray();
- }
- function displayArray() {
+}
+
+function displayArray() {
     const arrayTitle = document.getElementById('array-stack-title');
     arrayTitle.innerHTML = '';
 
     const heading = document.createElement('h2');
-    heading.textContent = 'arrayStack Visualization';
+    heading.textContent = 'ArrayStack Visualization';
     arrayTitle.appendChild(heading);
 
     const arrayDisplay = document.getElementById('array-display');
     arrayDisplay.innerHTML = ''; // Clear existing content
 
-    arrayStack.data.forEach((element) => {
-        // Skip undefined elements
-        if (element !== undefined) {
-            const elementContainer = document.createElement('div');
-            elementContainer.className = 'array-element';
-            elementContainer.textContent = `${element}`;
-            arrayDisplay.appendChild(elementContainer);
+    const isVerticalLayout = arrayDisplay.classList.contains('vertical-layout');
+
+    // Function to create and append element container
+    const appendElement = (element) => {
+        const elementContainer = document.createElement('div');
+        elementContainer.className = 'array-element';
+        elementContainer.textContent = `${element}`;
+        arrayDisplay.appendChild(elementContainer);
+    };
+
+    // Iterate based on layout
+    if (isVerticalLayout) {
+        for (let i = arrayStack.data.length - 1; i >= 0; i--) {
+            if (arrayStack.data[i] !== undefined) {
+                appendElement(arrayStack.data[i]);
+            }
         }
-    });
+    } else {
+        arrayStack.data.forEach(element => {
+            if (element !== undefined) {
+                appendElement(element);
+            }
+        });
+    }
 }
+
+// Toggle layout event listener
+document.getElementById('toggle-layout-btn').addEventListener('click', () => {
+    arrayDisplay.classList.toggle('vertical-layout');
+    displayArray(); // Update display on toggle
+});
+
 document.getElementById('clear-array-btn').addEventListener('click', () => {
     
     arrayStack.clear();
     updateDisplay();
     //toggleSidebar(false); // Revert sidebar when array is cleared
 });
+
 
 updateDisplay()
